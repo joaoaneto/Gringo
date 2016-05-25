318c842fdb6ec4f507b3908eb21ecf58dcac7727
Lucas Feijó Portela <lucasfjportela@gmail.com>
2016-05-24 22:57:04 -0300

    Todo o código passado para o Inglês(EN-US) e exclusão de classes duplicadas

--- classes_gringo.hpp
+++ classes_gringo.hpp
@@ -5,332 +5,303 @@
 #include <string>
 using namespace std;
 
-class Node{
+class Node {
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
-class Programa: public Node {	
+class Program : public Node {	
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
-class ListaExp: public Programa {
+class ExpList : public Program {
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
-class Comando: public ListaExp{
+class Command : public ExpList {
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
-class Exp: public Comando{
+class Exp : public Command {
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
-class Factor: public Exp{
+class Factor: public Exp {
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
-class ExpUn: public Factor{
+class UnExp: public Factor {
 	void accept(Visitor *v){
 		v->visit(this);
 	}
 };
 
 
-class Valor : public ExpUn{
+class Value : public UnExp {
 	void accept(Visitor *v){
 		v->visit(this);	
 	}
 };
 
-class Contexto {
-private:
-	static Contexto *instancia;
-	static Programa *programa;
-	Contexto(){}
-public:
-	typedef map<string, Valor*> TipoTabela;
-	static Contexto &getContexto() {
-		if (instancia==NULL){
-			instancia = new Contexto();
+class Context {
+	private:
+		static Context *instance;
+		static Program *program;
+		Context(){}
+	public:
+		typedef map<string, Value *> TypeTable;
+		static Context &getContext() {
+			if (instance == NULL){
+				instance = new Context();
+			}
+			return *instance;
 		}
-		return *instancia;
-	}
-	TipoTabela &getTabela(){
-		return this->tabela;
-	}
-	void setPrograma(Programa *prog){
-		programa = prog;
-	}
-	
-
-private:
-	TipoTabela tabela;
+		TypeTable &getTable(){
+			return this->table;
+		}
+		void setProgram(Program *prog){
+			program = prog;
+		}
+	private:
+		TypeTable table;
 };
 
-Contexto *Contexto::instancia = NULL;
+Context *Context::instance = NULL;
 
-class Comandos: public ListaExp{
-private:
-	ListaExp *listaExp;
-	Comando *comando;
-public:
-	Comandos(ListaExp *l, Comando *c): listaExp(l), comando(c){}
-	ListaExp *getListaExp(){
-		return this->listaExp;
-	}
-	Comando *getComando(){
-		return this->comando;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class Commands: public ExpList {
+	private:
+		ExpList *expList;
+		Command *command;
+	public:
+		Commands(ExpList *el, Command *c): expList(el), command(c){}
+		ExpList *getExpList(){
+			return this->expList;
+		}
+		Command *getCommand(){
+			return this->command;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class ExpBinPlus: public Exp{
-private:
-	Exp *exp;
-	Factor *factor;
-public:
-	ExpBinPlus(Exp *e, class Factor *f): exp(e), factor(f){}
-	Exp *getExp(){
-		return this->exp;
-	}
-	Factor *getFactor(){
-		return this->factor;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class BinExpPlus: public Exp {
+	private:
+		Exp *exp;
+		Factor *factor;
+	public:
+		BinExpPlus(Exp *e, class Factor *f): exp(e), factor(f){}
+		Exp *getExp(){
+			return this->exp;
+		}
+		Factor *getFactor(){
+			return this->factor;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class ExpBinMinus: public Exp{
-private:
-	Exp *exp;
-	Factor *factor;
-public:
-	ExpBinMinus(Exp *e, Factor *f): exp(e), factor(f){}
-	Exp *getExp(){
-		return this->exp;
-	}
-	Factor *getFactor(){
-		return this->factor;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class BinExpMinus: public Exp {
+	private:
+		Exp *exp;
+		Factor *factor;
+	public:
+		BinExpMinus(Exp *e, Factor *f): exp(e), factor(f){}
+		Exp *getExp(){
+			return this->exp;
+		}
+		Factor *getFactor(){
+			return this->factor;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class FactorMul: public Factor{
-private:
-	Factor *factor;
-	ExpUn *expUn;
-public:
-	FactorMul(Factor *f, ExpUn *eUn): factor(f), expUn(eUn){}
-	Factor *getFactor(){
-		return this->factor;
-	}
-	ExpUn *getExpUn(){
-		return this->expUn;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class FactorMul: public Factor {
+	private:
+		Factor *factor;
+		UnExp *unExp;
+	public:
+		FactorMul(Factor *f, unExp *ue): factor(f), unExp(ue){}
+		Factor *getFactor(){
+			return this->factor;
+		}
+		UnExp *getUnExp(){
+			return this->unExp;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class FactorDiv: public Factor{
-private:
-	Factor *factor;
-	ExpUn *expUn;
-public:
-	FactorDiv(Factor *f, ExpUn *eUn): factor(f), expUn(eUn){}
-	Factor *getFactor(){
-		return this->factor;
-	}
-	ExpUn *getExpUn(){
-		return this->expUn;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class FactorDiv: public Factor {
+	private:
+		Factor *factor;
+		UnExp *unExp;
+	public:
+		FactorDiv(Factor *f, UnExp *ue): factor(f), unExp(ue){}
+		Factor *getFactor(){
+			return this->factor;
+		}
+		UnExp *getUnExp(){
+			return this->unExp;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class PlusValue: public ExpUn{
-private:	
-	Valor *valor;
-public:
-	PlusValue(Valor *value): valor(value){}
-	Valor *getValor(){
-		return this->valor;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
+class PlusValue: public UnExp {
+	private:	
+		Value *value;
+	public:
+		PlusValue(Valor *v): value(v){}
+		Value *getValue(){
+			return this->value;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
 	}
 };
 
-class MinusValue: public ExpUn{
-private:
-	Valor *valor;
-public:
-	MinusValue(Valor *value): valor(value){}
-	Valor *getValor(){
-		return this->valor;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class MinusValue: public UnExp {
+	private:
+		Value *value;
+	public:
+		MinusValue(Value *v): value(v){}
+		Value *getValue(){
+			return this->value;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class LogValue: public ExpUn{
-private:
-	Exp *exp;
-public:
-	LogValue(Exp *e): exp(e){}
-	Exp *getExp(){
-		return this->exp;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class LogValue: public UnExp {
+	private:
+		Exp *exp;
+	public:
+		LogValue(Exp *e): exp(e){}
+		Exp *getExp(){
+			return this->exp;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class ExpValue: public ExpUn{
-private:
-	Exp *exp;
-public:
-	ExpValue(Exp *e): exp(e){}
-	Exp *getExp(){
-		return this->exp;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class ExpValue: public UnExp {
+	private:
+		Exp *exp;
+	public:
+		ExpValue(Exp *e): exp(e){}
+		Exp *getExp(){
+			return this->exp;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class Atribuicao : public Comando {
-private:
-	Exp *exp;
-	IdValue *idValue;
-public:
-	Atribuicao(Exp *e) : exp(e){}
-	Exp *getExp(){
-		return this->exp;
-	}
-	IdValue *getIdValue(){
-		return this->idValue;
-	}
-}; 
-
-
-class LparExpRpar : public Valor{
-private:
-	Exp *exp;
-public:
-	ValorPAR(Exp *e) : exp(e){}
-	Exp *getExp(){
-		return this->exp;
-	}
+class IntValue: public Value {
+	public:
+		int getValue() const{
+			return this->value;
+		}
+		IntValue(int value):value(value){} //Construtor
+		void accept(Visitor *v){
+			v->visit(this);
+		}
+	private:
+		int value;
 };
 
-class IntValue: public Valor{
-public:
-	int getValue() const{
-		return this->value;
-	}
+class DoubleValue: public Value {
+	public:
+		double getValue() const{
+			return this->value;
+		}
 
-	IntValue(int value):value(value){} //Construtor
-	void accept(Visitor *v){
-		v->visit(this);
-	}
-private:
-	int value;
+		DoubleValue(double value):value(value){} //Construtor
+		void accept(Visitor *v){
+			v->visit(this);
+		}
+	private:
+		double value;
 };
 
-class DoubleValue: public Valor{
-public:
-	double getValue() const{
-		return this->value;
-	}
-
-	DoubleValue(double value):value(value){} //Construtor
-	void accept(Visitor *v){
-		v->visit(this);
-	}
-private:
-	double value;
+class IdValue: public Value {
+	public:
+		char *getValue() const{
+			return this->idValue;
+		}
+		IdValue(char *idValue):idValue(idValue){}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
+	private:
+		char *idValue;
 };
 
-class IdValue: public Valor{
-public:
-	char *getValue() const{
-		return this->idValue;
-	}
-	IdValue(char *idValue):idValue(idValue){}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
-private:
-	char *idValue;
-};
-
-class LparExpRpar : public Valor{
-private:
-	Exp *exp;
-public:
-	LparExpRpar(Exp *e) : exp(e){}
+class LparExpRpar : public Value {
+	private:
+		Exp *exp;
+	public:
+		LparExpRpar(Exp *e) : exp(e){}
 
 		Exp *getExp(){
-		return this->exp;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+			return this->exp;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 };
 
-class Atribuicao : public Comando {
-private:
-	Exp *exp;
-	IdValue *idValue;
-public:
-	Atribuicao(Exp *e) : exp(e){}
-
-	Exp *getExp(){
-		return this->exp;
-	}
-	IdValue	*getIdValue(){
-		return this->idValue;
-	}
-	void accept(Visitor *v){
-		v->visit(this);
-	}
+class Atribuition : public Command {
+	private:
+		Exp *exp;
+		IdValue *idValue;
+	public:
+		Atribuition(Exp *e) : exp(e){}
+		Exp *getExp(){
+			return this->exp;
+		}
+		IdValue	*getIdValue(){
+			return this->idValue;
+		}
+		void accept(Visitor *v){
+			v->visit(this);
+		}
 }; 
 
 class Visitor {
-	virtual void visit(Comandos *) = 0;
-	virtual void visit(ExpBinPlus *) = 0;
-	virtual void visit(ExpBinMinus *) = 0;
+	virtual void visit(Commands *) = 0;
+	virtual void visit(BinExpPlus *) = 0;
+	virtual void visit(BinExpMinus *) = 0;
 	virtual void visit(FactorMul *) = 0;
 	virtual void visit(FactorDiv *) = 0;
-	virtual void visit(ExpUnPlus *) = 0;
-	virtual void visit(ExpUnMinus *) = 0;
-	virtual void visit(ExpUnLog *) = 0;
-	virtual void visit(ExpUnExp *) = 0;
+	virtual void visit(UnExpPlus *) = 0;
+	virtual void visit(UnExpMinus *) = 0;
+	virtual void visit(UnExpLog *) = 0;
+	virtual void visit(UnExpExp *) = 0;
 	virtual void visit(IntValue *) = 0;
 	virtual void visit(DoubleValue *) = 0;
 	virtual void visit(IdValue *) = 0;
 	virtual void visit(LparExpRpar *) = 0;
-	virtual void visit(Atribuicao *) = 0;
+	virtual void visit(Atribuition *) = 0;
 };
 
 #endif
