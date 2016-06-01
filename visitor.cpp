c238a58c523939ef908c4095fa8daf3b4db5957e
João Neto <j.alexandreeneto@gmail.com>
2016-06-01 00:46:49 -0300

    Revert "adição da regra if"
    This reverts commit e2916959864ceb5571b0eb1fd3623afb115788cb.

--- visitor.cpp
+++ visitor.cpp
@@ -34,9 +34,6 @@ void Operations::visit(Assignment *a){
 void Operations::visit(If *i){
 	i->If::getExp()->accept(this);
 	i->If::getExpList()->accept(this);
-
-	//falta terminar
-
 }
 
 void Operations::visit(BinExpPlus *bep){
